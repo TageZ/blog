1352829304a796798ee791bf06bba6365d17599a
TageZ <tagezerby@gmail.com>
2024-09-18 10:15:40 -0500

    Working on title and navigation

---
+++ src/Navbar.tsx
@@ -0,0 +1,15 @@
+
+function Navbar(){
+
+    return (
+        <div className="navbar">
+            <div className="header-box">
+                <h1 className="blog-title"> {/*Could change to an image for the title*/}
+                    Blog Title
+                </h1>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar;
