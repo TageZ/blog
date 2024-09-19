9e9e8b035b759fd54f9fde15a3865dbb74f430cc
TageZ <tagezerby@gmail.com>
2024-09-19 10:52:06 -0500

    Title?

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -5,7 +5,7 @@ function Navbar(){
         <div className="navbar">
             <div className="header-box">
                 <h1 className="blog-title"> {/*Could change to an image for the title*/}
-                    Blog Title
+                    The Voice of One Crying Out on the Internet
                 </h1>
             </div>
         </div>
